91cd3879865f4aa5342958303af6fe070c738334
Harry Sadler <harrysadlermusic@gmail.com>
2018-06-17 11:00:45 -0700

    add db scripts for adding difficulty rank to english word tables

--- app/server/scripts/populate/add_difficulty_rank_word_2.py
+++ app/server/scripts/populate/add_difficulty_rank_word_2.py
@@ -9,7 +9,7 @@ from utils.print import ppp
 # save with additional QWERTY difficulty rank value
 
 db = MySqlDriver(database_name='zentype')
-sql = 'SELECT * FROM english_word_1 ORDER BY qwerty_difficulty'
+sql = 'SELECT * FROM english_word_2 ORDER BY qwerty_difficulty'
 word_datas = db.query(sql)
 
 curr_difficulty_rank = 1
@@ -18,7 +18,7 @@ for word_data in word_datas:
 	word_data['qwerty_difficulty_rank'] = curr_difficulty_rank
 	word_DO = WordDataObject.create(
 		prop_dict=word_data,
-		language=WordDataObject.ENGLISH_1
+		language=WordDataObject.ENGLISH_2
 	)
 	save_res = word_DO.save()
 	ppp(save_res)
