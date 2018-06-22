4f422d4fe2a541cc94b31525c27638cb43eeeecd
Harry Sadler <harrysadlermusic@gmail.com>
2018-06-21 23:37:11 -0700

    create Word service, add notes

--- app/server/service/word.py
+++ app/server/service/word.py
@@ -1,21 +1,27 @@
 
 # Word Service
 
-from service.keyboard import Keyboard
-
 
 class Word():
 	"""
 	Word Service
 
+	TODO:
+		- decide if all methods should be static
+		- stub out methods
+		- decide hierarchy with typing_test service
+
 	"""
 
 
-	def __init__(self, text):
-		self.text = text
-
-	def get_difficulty(self):
+	@classmethod
+	def get_random(
+		language,
+		difficulty=None,
+		frequency=None,
+		length=None,
+		substring=None
+	):
 		pass
-
 
 
