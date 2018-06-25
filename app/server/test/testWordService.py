7ca2ca302316d26e30a59af81f4cf933bcf9b0c2
Harry Sadler <harrysadlermusic@gmail.com>
2018-06-25 09:01:10 -0700

    add query_bind method to mysql_driver, start implementation of word service

---
+++ app/server/test/testWordService.py
@@ -0,0 +1,39 @@
+
+import sys
+sys.path.append('..')
+
+from data_object.word_data_object import WordDataObject
+from service.word import Word
+from utils.print import ppp
+
+
+# random word list options
+language = WordDataObject.ENGLISH
+qwerty_difficulty_rank = {
+	'min': 0,
+	'max': 100
+}
+frequency_rank = {
+	'min': 0,
+	'max': 100
+}
+length = {
+	'min': 0,
+	'max': 20
+}
+substring = 'cat'
+limit = 10
+
+
+word_list = Word.get_random_list(
+	language=language,
+	qwerty_difficulty_rank=qwerty_difficulty_rank,
+	frequency_rank=frequency_rank,
+	length=length,
+	substring=substring,
+	limit=limit,
+)
+
+
+ppp(word_list)
+
