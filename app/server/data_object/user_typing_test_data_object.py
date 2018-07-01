82ea77f0bb8e33b6752753f2fc884570bbcce024
Harry Sadler <harrysadlermusic@gmail.com>
2018-07-01 10:28:18 -0700

    add tables, stub data objects for user_typing_test

--- app/server/data_object/user_typing_test_data_object.py
+++ app/server/data_object/user_typing_test_data_object.py
@@ -5,16 +5,14 @@ from data_store.database_driver.mysql_driver import MySqlDriver
 from data_store.cache_driver.redis_driver import RedisDriver
 
 
-class TypingTestDataObject(BaseDataObject):
+class UserTypingTestDataObject(BaseDataObject):
 	"""
-	Typing Test Data Object
+	User Typing Test Data Object
 
 	"""
 
 
-	TABLE_NAME = 'typing_test'
+	TABLE_NAME = 'user_typing_test'
 	DEFAULT_DB_DRIVER_CLASS = MySqlDriver
 	DEFAULT_CACHE_DRIVER_CLASS = RedisDriver
 	DEFAULT_CACHE_TTL = 3600
-
-	LANGUAGE_ENGLISH = 1
--- app/server/scripts/create-mysql-table.py
+++ app/server/scripts/create-mysql-table.py
@@ -23,9 +23,9 @@ mysql_driver = MySqlDriver(
 # 	)
 # """
 
-# create english_word_1 table
+# create english_word table
 # create_table_query = """
-# 	CREATE TABLE IF NOT EXISTS english_word_1 (
+# 	CREATE TABLE IF NOT EXISTS english_word (
 # 		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 # 		created_ts INT(11) NOT NULL,
 # 		updated_ts INT(11) NOT NULL,
@@ -70,6 +70,37 @@ mysql_driver = MySqlDriver(
 # 		typing_test_id INT(6) UNSIGNED NOT NULL,
 # 		word_id INT(6) UNSIGNED NOT NULL,
 # 		position INT(4) UNSIGNED NOT NULL
+# 	)
+# """
+
+# create user_typing_test table
+# create_table_query = """
+# 	CREATE TABLE IF NOT EXISTS user_typing_test (
+# 		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+# 		created_ts INT(11) NOT NULL,
+# 		updated_ts INT(11) NOT NULL,
+# 		# table specific columns here:
+# 		typing_test_id INT(6) UNSIGNED NOT NULL,
+# 		user_id INT(6) UNSIGNED NOT NULL,
+# 		status TINYINT(1) UNSIGNED NOT NULL,
+# 		test_time INT(4) UNSIGNED,
+# 		wpm INT(3) UNSIGNED
+# 	)
+# """
+
+# create user_typing_test_content table
+# create_table_query = """
+# 	CREATE TABLE IF NOT EXISTS user_typing_test_content (
+# 		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+# 		created_ts INT(11) NOT NULL,
+# 		updated_ts INT(11) NOT NULL,
+# 		# table specific columns here:
+# 		user_typing_test_id INT(6) UNSIGNED NOT NULL,
+# 		word_id INT(6) UNSIGNED NOT NULL,
+# 		position INT(4) UNSIGNED NOT NULL,
+# 		attempted TINYINT(1) UNSIGNED,
+# 		completed_correctly TINYINT(1) UNSIGNED,
+# 		completion_time INT(4)
 # 	)
 # """
 
